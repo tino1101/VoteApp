c3e36485273276eff3adf01ee2ab06cbfb4f0c2a
liujun <liujun@dachentech.com.cn>
2019-09-06 15:56:00 +0800

    optimize

--- app/src/main/java/com/tino/vote/widget/VoteView.kt
+++ app/src/main/java/com/tino/vote/widget/VoteView.kt
@@ -1,17 +1,12 @@
 package com.tino.vote.widget
 
-import android.animation.ObjectAnimator
+import android.animation.*
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.core.content.ContextCompat
 import com.tino.vote.R
-import com.tino.vote.util.UIUtil
 
 class VoteView(context: Context?, attrs: AttributeSet?) : FrameLayout(context, attrs) {
 
@@ -22,10 +17,11 @@ class VoteView(context: Context?, attrs: AttributeSet?) : FrameLayout(context, a
     private var mChose = false;
 
     init {
-        val view = inflate(context, R.layout.vote_layout, this)
-        titleView = view.findViewById(R.id.title_text_view)
-        optionsLayout = view.findViewById(R.id.options_layout)
-        totalNumberView = view.findViewById(R.id.total_number_text_view)
+        inflate(context, R.layout.vote_layout, this).run {
+            titleView = findViewById(R.id.title_text_view)
+            optionsLayout = findViewById(R.id.options_layout)
+            totalNumberView = findViewById(R.id.total_number_text_view)
+        }
     }
 
     fun setData(chose: Boolean, title: String, totalNumber: Int, options: List<OptionInfo>) {
@@ -37,7 +33,7 @@ class VoteView(context: Context?, attrs: AttributeSet?) : FrameLayout(context, a
         for (index in options.indices) {
             if (options[index].chose) current = index
             (inflate(context, R.layout.vote_option_layout, optionsLayout) as? LinearLayout)?.getChildAt(index)?.let {
-                var progressView: View = it.findViewById(R.id.progress_view)
+                var progressView: ProgressBar = it.findViewById(R.id.progress_view)
                 val nameView: TextView = it.findViewById(R.id.name_text_view)
                 val numberView: TextView = it.findViewById(R.id.number_text_view)
                 nameView.text = options[index].name
@@ -65,23 +61,24 @@ class VoteView(context: Context?, attrs: AttributeSet?) : FrameLayout(context, a
         optionsLayout.post {
             for (index in 0 until optionsLayout.childCount) {
                 optionsLayout.getChildAt(index)?.let {
-                    var progressView: View = it.getTag(R.string.tag_progress_view) as View
+                    var progressView: ProgressBar = it.getTag(R.string.tag_progress_view) as ProgressBar
                     val nameView: TextView = it.getTag(R.string.tag_name_view) as TextView
                     val numberView: TextView = it.getTag(R.string.tag_number_view) as TextView
+                    it.isSelected = current == index
                     progressView.isSelected = current == index
-                    progressView.background = if (it.width - (options[index].number.toFloat() / totalNumber.toFloat()) * it.width > UIUtil.dip2px(context, 5f)) {
-                        ContextCompat.getDrawable(context, R.drawable.progress_view_bg)
-                    } else {
-                        ContextCompat.getDrawable(context, R.drawable.progress_view_bg2)
-                    }
-                    nameView.setTextColor(if (current == index) Color.parseColor("#2B8DE2") else Color.parseColor("#999999"))
+                    nameView.setTextColor(if (current == index) Color.parseColor("#2B8DE2") else Color.parseColor("#777777"))
                     numberView.setTextColor(nameView.currentTextColor)
-                    ObjectAnimator.ofInt(ViewWrapper(progressView), "customWidth", ((options[index].number.toFloat() / totalNumber.toFloat()) * it.width).toInt()).setDuration(1000).start()
-                    if (nameView.width > it.width - numberView.width - UIUtil.dip2px(context, 30f)) {
-                        ObjectAnimator.ofInt(ViewWrapper(nameView), "customWidth", nameView.width, it.width - numberView.width - UIUtil.dip2px(context, 30f)).setDuration(1000).start()
+                    AnimatorSet().run {
+                        playTogether(arrayListOf<Animator>(ObjectAnimator.ofInt(progressView, "progress", (options[index].number.toFloat() / totalNumber.toFloat() * 100).toInt()),
+                            ObjectAnimator.ofFloat(nameView, "x",  resources.getDimension(R.dimen.name_view_left_margin)),
+                            ObjectAnimator.ofFloat(numberView, "alpha", 1f)).apply {
+                            if (nameView.width > it.width - numberView.width - resources.getDimension(R.dimen.number_view_right_margin) - resources.getDimension(R.dimen.name_view_left_margin) - resources.getDimension(R.dimen.name_view_number_view_margin)) {
+                                add(ObjectAnimator.ofInt(ViewWrapper(nameView), "customWidth", nameView.width, it.width - numberView.width - (resources.getDimension(R.dimen.number_view_right_margin) + resources.getDimension(R.dimen.name_view_left_margin) + resources.getDimension(R.dimen.name_view_number_view_margin)).toInt()))
+                            }
+                        })
+                        duration = 800
+                        start()
                     }
-                    ObjectAnimator.ofFloat(nameView, "X",  UIUtil.dip2px(context, 10f).toFloat()).setDuration(1000).start()
-                    ObjectAnimator.ofFloat(numberView, "alpha", 1f).setDuration(1000).start()
                 }
             }
         }
